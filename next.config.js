b96f7a0f8c3a2657fa12f5f9e78d018523aa9105
kamalkumar7 <202051098@iiitvadodara.ac.in>
2023-07-26 03:02:26 +0530

    first commit

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: [
+            'avatars.githubusercontent.com',
+      'res.cloudinary.com', 
+      'lh3.googleusercontent.com'
+    ]
+  }
+}
+
+module.exports = nextConfig
